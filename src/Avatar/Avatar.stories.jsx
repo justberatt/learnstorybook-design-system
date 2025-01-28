3eb5169018a21a6fc1f1ffe0d28697826be1ec6f
Berat Sakipi <berat.sakipi@gmail.com>
2025-01-28 03:24:29 +0100

    a lot has changed, don't even ask me

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -3,6 +3,15 @@ import { Avatar } from './Avatar';
 export default {
   title: 'Design System/Avatar',
   component: Avatar,
+
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['tiny', 'small', 'medium', 'large']
+    }
+  }
 };
 
 export const Standard = {
@@ -65,4 +74,13 @@ export const Large = {
       />
     </>
   ),
+};
+
+export const Controls = {
+  args: {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Nguyen',
+    src: 'https://avatars.githubusercontent.com/u/263385',
+  },
 };
